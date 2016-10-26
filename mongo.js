df433216f33fd647f4437032c6e618ced123a6f3
Yoshi T. Munchakoopas <yoshi.t.munchakoopas@hp.com>
2016-10-26 09:01:50 +0000

    Refactoring

---
+++ mongo.js
@@ -0,0 +1,16 @@
+
+/**
+ *
+ */
+
+var sg              = require('sgsg');
+var _               = sg._;
+
+_.each(require('./lib/read'), function(value, key) {
+  exports[key] = value;
+});
+
+_.each(require('./lib/write'), function(value, key) {
+  exports[key] = value;
+});
+
